a828a6f7b87bfa3211d7d6575f50e4270b39967c
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-10-27 20:27:22 +0700

    setup: update data module declaration

--- src/data/history.h
+++ src/data/history.h
@@ -1,3 +1,15 @@
+#ifndef DATA_HISTORY_H
+#define DATA_HISTORY_H
+
+#include "../adt/stack_command.h"
+
+extern StackState history;
+extern StackState restore;
+
+void initialize_history();
+
+#endif
+
 // buat action stack sebagai sumber backup state
 // buat restore stack untuk action redo
 // restore stack akan direset ketika user melakukan undo, lalu melakukan aksi yang memiliki side effect
