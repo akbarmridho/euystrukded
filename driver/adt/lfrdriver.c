750c0aca6c7b3a3a4e36d11bbb780dfcc58fed48
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2022-10-28 16:18:21 +0700

    feat: add list_food_recipe

---
+++ driver/adt/lfrdriver.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "../../src/adt/list_food_recipe.h"
+
+int main(){
+    food_t f1, f2, f3, f4;
+    create_food(&f1, 12, char_to_string("Sate ayam"), minute_to_time(120), minute_to_time(10), Mix);
+    create_food(&f2, 45, char_to_string("Ayam geprek"), minute_to_time(200), minute_to_time(15), Buy);
+    create_food(&f3, 26, char_to_string("Nasi goreng"), minute_to_time(150), minute_to_time(9), Fry);
+    create_food(&f4, 5, char_to_string("Jus Alpukat"), minute_to_time(100), minute_to_time(11), Mix);
+
+    recipe_t r1, r2, r3, r4;
+    int ingredients[5] = {0, 1, 2, 3, 4}; 
+    create_recipe(&r1, 12, 5, ingredients);
+    create_recipe(&r2, 45, 5, ingredients);
+    create_recipe(&r3, 26, 5, ingredients);
+    create_recipe(&r4, 5, 5, ingredients);
+
+    food_recipe_t fr1, fr2, fr3, fr4, val;
+    create_food_recipe(&fr1, f1, r1); 
+    create_food_recipe(&fr2, f2, r2); 
+    create_food_recipe(&fr3, f3, r3); 
+    create_food_recipe(&fr4, f4, r4); 
+
+    /* print kosong */
+    ListFoodRecipe lfr;
+    create_lfr(&lfr);
+    display_lfr(lfr);
+    printf("(empty)\n\n");
+
+
+    lfr_insert_first(&lfr, fr1);
+    lfr_insert_last(&lfr, fr3);
+    lfr_insert_at(&lfr, fr2, 1);
+    lfr_insert_at(&lfr, fr4, 0);
+    display_lfr(lfr);
+    putchar('\n');
+    /*
+     * expected order:
+     * Jus Alpukat
+     * Sate ayam
+     * Ayam geprek
+     * Nasi goreng
+     */
+
+    lfr_delete_at(&lfr, &val, 3);   /* delete last test */
+    lfr_delete_at(&lfr, &val, 1);   /* delete test */
+    lfr_delete_at(&lfr, &val, 0);   /* delete first test*/
+    display_lfr(lfr);
+    /*
+     * expected order:
+     * Ayam geprek
+     */
+
+    return 0;
+}
--- src/adt/list_food_recipe.h
+++ src/adt/list_food_recipe.h
@@ -24,22 +24,30 @@ typedef struct
    recipe_t recipe;
 } food_recipe_t;
 
-void create_food_recipe(food_t food, recipe_t recipe);
+/* SELEKTOR food_recipe_t */
+#define FR_FOOD(fr) (fr).food
+#define FR_RECIPE(fr) (fr).recipe
 
+/* KONSTRUKTOR food_recipe_t */
+void create_food_recipe(food_recipe_t *fr, food_t food, recipe_t recipe);
+
+
+/* *** LIST FOOD RECIPE *** */
 /* Definisi elemen dan koleksi objek */
 typedef int IdxType;
 typedef struct
 {
    food_recipe_t contents[CAPACITY]; /* memori tempat penyimpan elemen (container) */
+   int neff;  /* banyak elemen efektif*/
 } ListFoodRecipe;
 /* Indeks yang digunakan [0..CAPACITY-1] */
-/* Jika l adalah ListFoodRecipe, cara deklarasi dan akses: */
-/* Deklarasi : l : ListFoodRecipe */
+/* Jika lfr adalah ListFoodRecipe, cara deklarasi dan akses: */
+/* Deklarasi : lfr : ListFoodRecipe */
 /* Maka cara akses:
-   ELMT(l,i) untuk mengakses elemen ke-i */
+   ELMT(lfr,i) untuk mengakses elemen ke-i */
 /* Definisi :
-   List kosong: semua elemen bernilai MARK
-   Definisi elemen pertama: ELMT(l,i) dengan i=0 */
+   List kosong: neff = 0
+   Definisi elemen pertama: ELMT(lfr,i) dengan i=0 */
 
 /* Ke depannya, ListFoodRecipe, list food recipe, dll
 disingkat menjadi lfr
@@ -47,57 +55,102 @@ disingkat menjadi lfr
 
 /* ********** SELEKTOR ********** */
 #define LFR_ELMT(l, i) (l).contents[(i)]
+#define LFR_NEFF(l) (l).neff
 
-/* I.S. l sembarang
-F.S. Terbentuk List l kosong dengan kapasitas CAPACITY
-Proses: Inisialisasi semua elemen List l dengan MARK */
-void create_lfr(ListFoodRecipe *l);
+/* I.S. lfr sembarang
+F.S. Terbentuk List lfr kosong dengan kapasitas CAPACITY
+Proses: Inisialisasi semua elemen List lfr dengan MARK */
+void create_lfr(ListFoodRecipe *lfr);
 
 /*Banyaknya elemen
 Mengirimkan banyaknya elemen efektif List
 Mengirimkan nol jika List kosong */
-int lfr_length(ListFoodRecipe l);
+int lfr_length(ListFoodRecipe lfr);
 
 /*
-Prekondisi : List l tidak kosong
-Mengirimkan indeks elemen l pertama */
-IdxType lfr_get_first_idx(ListFoodRecipe l);
+Prekondisi : List lfr tidak kosong
+Mengirimkan indeks elemen lfr pertama */
+IdxType lfr_get_first_idx(ListFoodRecipe lfr);
 
 /*
-Prekondisi : List l tidak kosong
-Mengirimkan indeks elemen l terakhir */
-IdxType lfr_get_last_idx(ListFoodRecipe l);
+Prekondisi : List lfr tidak kosong
+Mengirimkan indeks elemen lfr terakhir */
+IdxType lfr_get_last_idx(ListFoodRecipe lfr);
 
 /*
-Mengirimkan true jika i adalah indeks yang valid utk kapasitas List l
+Mengirimkan true jika i adalah indeks yang valid utk kapasitas List lfr
 yaitu antara indeks yang terdefinisi utk container
 */
-boolean lfr_is_idx_valid(ListFoodRecipe l, IdxType i);
+boolean lfr_is_idx_valid(ListFoodRecipe lfr, IdxType i);
 
 /*
-Mengirimkan true jika i adalah indeks yang terdefinisi utk List l
-yaitu antara 0..length(l)-1
+Mengirimkan true jika i adalah indeks yang terdefinisi utk List lfr
+yaitu antara 0..length(lfr)-1
 */
-boolean lfr_is_idx_eff(ListFoodRecipe l, IdxType i);
+boolean lfr_is_idx_eff(ListFoodRecipe lfr, IdxType i);
 
-/* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
-boolean lfr_is_empty(ListFoodRecipe l);
+/* Mengirimkan true jika List lfr kosong, mengirimkan false jika tidak */
+boolean lfr_is_empty(ListFoodRecipe lfr);
 
-/* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
-boolean lfr_is_full(ListFoodRecipe l);
+/* Mengirimkan true jika List lfr penuh, mengirimkan false jika tidak */
+boolean lfr_is_full(ListFoodRecipe lfr);
 
 /* Proses: Menambahkan val sebagai elemen pada index idx List */
-/* I.S. List l tidak kosong dan tidak penuh, idx merupakan index yang valid di l */
-/* F.S. val adalah elemen yang disisipkan pada index idx l */
-/* *** Menambahkan elemen terakhir *** */
-void lfr_insert_at(ListFoodRecipe *l, food_recipe_t val, IdxType idx);
+/* I.S. List lfr tidak penuh, idx merupakan index yang efektif di lfr */
+/* F.S. val adalah elemen yang disisipkan pada index idx lfr */
+/* *** Menambahkan elemen pada idx *** */
+void lfr_insert_at(ListFoodRecipe *lfr, food_recipe_t val, IdxType idx);
+
+/* Proses: Menambahkan val sebagai elemen pada index awal List */
+/* I.S. List lfr tidak penuh */
+/* F.S. val adalah elemen yang disisipkan pada awal lfr */
+/* *** Menambahkan elemen pada idx *** */
+void lfr_insert_first(ListFoodRecipe *lfr, food_recipe_t val);
+
+/* Proses: Menambahkan val sebagai elemen pada index akhir List */
+/* I.S. List lfr tidak penuh */
+/* F.S. val adalah elemen yang disisipkan pada akhir lfr */
+/* *** Menambahkan elemen pada idx *** */
+void lfr_insert_last(ListFoodRecipe *lfr, food_recipe_t val);
 
 /* Proses : Menghapus elemen pada index idx List */
-/* I.S. List tidak kosong, idx adalah index yang valid di l */
-/* F.S. val adalah nilai elemen pada index idx l sebelum penghapusan, */
+/* I.S. List tidak kosong, idx adalah index yang efektif di lfr */
+/* F.S. val adalah nilai elemen pada index idx lfr sebelum penghapusan, */
 /*      Banyaknya elemen List berkurang satu */
-/*      List l mungkin menjadi kosong */
-/* *** Menghapus elemen terakhir *** */
-void lfr_delete_at(ListFoodRecipe *l, food_recipe_t *val, IdxType idx);
+/*      List lfr mungkin menjadi kosong */
+/* *** Menghapus elemen pada idx *** */
+void lfr_delete_at(ListFoodRecipe *lfr, food_recipe_t *val, IdxType idx);
+
+/* Proses : Menghapus elemen awal List */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen pada index 0 lfr sebelum penghapusan, */
+/*      Banyaknya elemen List berkurang satu */
+/*      List lfr mungkin menjadi kosong */
+/* *** Menghapus elemen pada awal lfr *** */
+void lfr_delete_first(ListFoodRecipe *lfr, food_recipe_t *val);
+
+/* Proses : Menghapus elemen terakhir List */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen pada index idx lfr sebelum penghapusan, */
+/*      Banyaknya elemen List berkurang satu */
+/*      List lfr mungkin menjadi kosong */
+/* *** Menghapus elemen pada akhir lfr *** */
+void lfr_delete_last(ListFoodRecipe *lfr, food_recipe_t *val);
+
+
+/* OUTPUT/DISPLAY */
+/*
+ * display_lfr
+ * I.S: lfr sembarang
+ * F.S: lfr tercetak dengan format
+ * List Resep
+ * (aksi yang diperlukan - bahan...)
+ *    1. Ayam Goreng
+ *       FRY - Ayam Potong - Minyak Goreng
+ *    2. Telur Rebus
+ *       BOIL - Telur
+ *    ...
+ */
+void display_lfr(ListFoodRecipe lfr);
 
 #endif
