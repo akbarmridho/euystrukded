36a53f65e3d7dc7d7bade38a307becd053ce3322
egijago <egiyangbaik@gmail.com>
2022-10-28 14:01:22 +0700

    feat: adt food

--- src/adt/food.h
+++ src/adt/food.h
@@ -19,9 +19,25 @@ typedef struct
     string name;
     time_t expire_time;
     time_t delivery_time;
-    food_source source;
+    enum food_source source;
 } food_t;
 
-void create_food(food_t *f, word_t name, time_t expire_time, time_t delivery_time, food_source source);
+/* SELEKTOR */
+#define ID(f) (f).id
+#define NAME(f) (f).name
+#define EXPIRE_TIME(f) (f).expire_time
+#define DELIVERY_TIME(f) (f).delivery_time
+#define SOURCE(f) (f).source
+
+/* KONSTRUKTOR */
+void create_food(food_t *f,int id, word_t name, time_t expire_time, time_t delivery_time, enum food_source source);
+// I.S. f sembarang
+// F.S  f terdefinisi dengan atribut sesuai argumen
+
+
+void display_food(food_t food);
+// I.S. food terdefinisi
+// F.S  menampilkan atribut food pada layar
+
 
 #endif
