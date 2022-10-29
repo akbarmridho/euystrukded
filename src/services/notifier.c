4399ce57efdd61a2ac6aab0d983212be5773b6e2
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-10-29 12:39:50 +0700

    fix: display notification update message

--- src/services/notifier.c
+++ src/services/notifier.c
@@ -2,23 +2,21 @@
 
 simulator_t simulator;
 
-void notify(string notification)
-{
+void notify(string notification) {
     n_insert_last(&simulator.notification, notification);
 }
 
-void clear_notification()
-{
+void clear_notification() {
     n_delete_list(&simulator.notification);
 }
 
-void display_notification()
-{
+void display_notification() {
     if (n_list_length(simulator.notification) == 0) {
         printf("Notifikasi: -\n");
     } else {
+        printf("Notifikasi:\n");
         for (int i = 0; i < n_list_length(simulator.notification); i++) {
-            printf("    %d. ", i+1);
+            printf("    %d. ", i + 1);
             print_string(notif(simulator.notification, i));
             putchar('\n');
         }
