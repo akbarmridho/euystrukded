0ba35bb313e2bcf6a069fab3576ed23d6219055d
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2022-10-29 14:45:03 +0700

    feat: chopper, boiler services

--- src/services/boiler.h
+++ src/services/boiler.h
@@ -1,6 +1,27 @@
+#ifndef SERVICE_BOILER_H
+#define SERVICE_BOILER_H
+
+#include "clock.h"
+#include "../data/simulator.h"
+#include "../data/configdata.h"
+#include "../adt/boolean.h"
+
+extern simulator_t simulator;
+extern ListFoodRecipe food_recipe;
+
 /*
-ambil makanan dari inventory
-panggil ticker
-tambahkan hasil ke inventory
+ * BOILER CHECK
+ * mengecek apakah bahan baku tersedia untuk melakukan proses boil
+ */
+boolean can_boil_food(int result_id);
+
+/*
+ * BOILER SERVICE (procedure boil())
+ * dipanggil apabila bahan mencukupi
+ * proses:
+ * ambil bahan dari inventory (dequeue)
+ * panggil ticker
+ * enqueue hasil ke inventory
 */
-void boil();
+void boil(int result_id);
+#endif
