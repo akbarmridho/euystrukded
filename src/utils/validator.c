b70cd6e5ccd727d9e9cad7e7e4c165d60978bd8b
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-10-30 16:20:31 +0700

    update validator

--- src/utils/validator.c
+++ src/utils/validator.c
@@ -1,19 +1,20 @@
-#ifndef UTILS_VALIDATOR_h
-#define UTILS_VALIDATOR_h
+#ifndef UTILS_VALIDATOR_H
+#define UTILS_VALIDATOR_H
 
 #include "../adt/string.h"
 #include <stdio.h>
 
 /* inputs an integer x and returns x if min<=x<=max, else input x until x satisfies range */
-int validate_int(int min, int max, string prompt){
+int validate_int(int min, int max, string prompt) {
     int x;
     print_string(prompt);
-    scanf("%d", x);
     putchar('\n');
-    while (x < min || x > max){
+    scanf("%d", &x);
+
+    while (x < min || x > max) {
         print_string(prompt);
-        scanf("%d", x);
         putchar('\n');
+        scanf("%d", &x);
     }
     return x;
 }
