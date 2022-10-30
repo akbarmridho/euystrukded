c02779ee9d9256e7922b5f6a962a32f291610580
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-10-30 12:37:08 +0700

    feat: from branch adt egi

--- driver/adt/list_delivery_driver.c
+++ driver/adt/list_delivery_driver.c
@@ -1,6 +1,61 @@
-#include "../../src/adt/list_food.h"
+#include "../../src/adt/list_delivery.h"
 
-int main(){
+// gcc list_delivery_driver.c "../../src/adt/list_delivery.c" "../../src/adt/food.c" "../../src/adt/time.c" "../../src/adt/string.c" -o list_delivery
+int main() {
+    int capacity = 100;
+    ListDelivery l;
+    create_list_delivery(&l, capacity);
+    printf("%c\n", list_delivery_is_empty(l));
+
+    food_t f1, f2, f3, f4, f5;
+    create_food(&f1, 12, char_to_string("Sate ayam"), minute_to_time(120), minute_to_time(10), Mix);
+    create_food(&f2, 45, char_to_string("Ayam geprek"), minute_to_time(200), minute_to_time(15), Buy);
+    create_food(&f3, 26, char_to_string("Nasi goreng"), minute_to_time(150), minute_to_time(9), Fry);
+    create_food(&f4, 5, char_to_string("Jus Alpukat"), minute_to_time(100), minute_to_time(11), Mix);
+    create_food(&f5, 5, char_to_string("Jus Alpukat siap diantar"), minute_to_time(100), minute_to_time(0), Mix);
+
+    enqueue_delivery(&l, f1);
+    enqueue_delivery(&l, f2);
+    enqueue_delivery(&l, f3);
+    enqueue_delivery(&l, f4);
+    enqueue_delivery(&l, f5);
+    display_list_delivery(l);
+    printf("%d,%d,%d\n", list_delivery_get_idx_head(l), list_delivery_get_idx_tail(l), list_delivery_length(l));
+
+    printf("\n\n\n\n");
+
+    food_t dump;
+    dequeue_delivery(&l, &dump);
+    display_food(dump);
+    printf("\n");
+    display_list_delivery(l);
+    printf("\n\n\n\n");
+
+    ListDelivery copy_of_l;
+    list_delivery_copy(l, &copy_of_l);
+    display_list_delivery(copy_of_l);
+    printf("\n\n\n\n");
+
+    list_delivery_delete(&copy_of_l, 1);
+    display_list_delivery(copy_of_l);
+    printf("\n\n\n\n");
+
+    expand_list_delivery(&copy_of_l, 100);
+    for (int i = 0; i < 110; i++) {
+        ELMT(copy_of_l, i) = f1;
+    }
+    display_food(ELMT(copy_of_l, 109));
+    NEFF(copy_of_l) = 110;
+
+    printf("Capacity after expand: %d\n", CAPACITY(copy_of_l));
+
+    compress_list_delivery(&copy_of_l);
+
+    printf("Capacity after compress: %d\n", CAPACITY(copy_of_l));
+
+    deallocate_list_delivery(&copy_of_l);
+
+    display_list_delivery(copy_of_l);
 
     return 0;
 }
--- src/adt/list_food.c
+++ src/adt/list_food.c
@@ -3,6 +3,9 @@
 
 #include "boolean.h"
 #include "food.h"
+#include "list_food.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /*  Kamus Umum */
 #define IDX_MIN 0
@@ -12,11 +15,10 @@
 
 /* Definisi elemen dan koleksi objek */
 typedef int IdxType;
-typedef struct
-{
-   food_t * buffer; /* memori tempat penyimpan elemen (container) */
-   int nEff;       /* >=0, banyaknya elemen efektif */
-   int capacity;   /* ukuran elemen */
+typedef struct {
+    food_t *buffer; /* memori tempat penyimpan elemen (container) */
+    int nEff;       /* >=0, banyaknya elemen efektif */
+    int capacity;   /* ukuran elemen */
 } ListFood;
 /* Indeks yang digunakan [0..capacity-1] */
 /* Jika l adalah : ListFood, cara deklarasi dan akses: */
@@ -42,73 +44,74 @@ Konstruktor : create list kosong
 I.S. l sembarang, capacity > 0
 F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity
 */
-void create_list_food(ListFood *l, int capacity){
+void create_list_food(ListFood *l, int capacity) {
     CAPACITY(*l) = capacity;
     NEFF(*l) = 0;
-    BUFFER(*l) = malloc(capacity*sizeof(food_t));
+    BUFFER(*l) = malloc(capacity * sizeof(food_t));
 }
+
 /*
 I.S. l terdefinisi;
 F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
-void deallocate_list_food(ListFood *l){
+void deallocate_list_food(ListFood *l) {
     CAPACITY(*l) = 0;
     NEFF(*l) = 0;
     free(BUFFER(*l));
 }
+
 /*
 Banyaknya elemen
 Mengirimkan banyaknya elemen efektif list
 Mengirimkan nol jika list l kosong
 Daya tampung container *** */
-int list_food_length(ListFood l){
+int list_food_length(ListFood l) {
     return NEFF(l);
 }
 
 /* Mengirimkan true jika list l kosong, mengirimkan false jika tidak */
-boolean list_food_is_empty(ListFood l){
-    return (NEFF(l)==0);
+boolean list_food_is_empty(ListFood l) {
+    return (NEFF(l) == 0);
 }
 
 /* Mengirimkan true jika list l penuh, mengirimkan false jika tidak */
-boolean list_food_is_full(ListFood l){
+boolean list_food_is_full(ListFood l) {
     return NEFF(l) == CAPACITY(l);
-}   
+}
 
 /* I.S. lIn terdefinisi tidak kosong, lOut sembarang
 F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama)
 Proses : Menyalin isi lIn ke lOut */
-void list_food_copy(ListFood lIn, ListFood *lOut){
+void list_food_copy(ListFood lIn, ListFood *lOut) {
     deallocate_list_food(lOut);
-    create_list_food(lOut,CAPACITY(lIn));
+    create_list_food(lOut, CAPACITY(lIn));
     NEFF(*lOut) = NEFF(lIn);
-    for(int i = 0;i<list_food_length(lIn);i++){
-        ELMT(*lOut,i) = ELMT(lIn,i);
+    for (int i = 0; i < list_food_length(lIn); i++) {
+        ELMT(*lOut, i) = ELMT(lIn, i);
     }
 }
 
-void list_food_delete(ListFood * l , int idx){
-    for (int i= idx;i<list_food_length(*l)-1;i++){
-        ELMT(*l,i) = ELMT(*l,i+1);
+void list_food_delete(ListFood *l, int idx) {
+    for (int i = idx; i < list_food_length(*l) - 1; i++) {
+        ELMT(*l, i) = ELMT(*l, i + 1);
     }
     NEFF(*l)--;
 }
 
-/* Menghasilkan berapa banyak kemunculan food dengan nama yang sama dengan val di l
+/* Menghasilkan berapa banyak kemunculan food dengan ID yang sama dengan val di l
 Jika l kosong menghasilkan 0 */
-int list_food_count_val(ListFood l, food_t val){
+int list_food_count_val(ListFood l, food_t val) {
     int count = 0;
-    for (int i=0;i<list_food_length(l);i++){
-        // COMPARE STRING <nanti di ubah> 
-        if (comparestr(FOOD_NAME(ELMT(l,i)),FOOD_NAME(val))){
-            count ++;
+    for (int i = 0; i < list_food_length(l); i++) {
+        if (FOOD_ID(ELMT(l, i)) == FOOD_ID(val)) {
+            count++;
         }
     }
     return count;
 }
 
 /* IDX_UNDEF jika kosong, 0 jika tidk kosong */
-int list_food_get_idx_head(ListFood l){
-    if (list_food_is_empty(l)){
+int list_food_get_idx_head(ListFood l) {
+    if (list_food_is_empty(l)) {
         return IDX_UNDEF;
     } else {
         return 0;
@@ -116,12 +119,50 @@ int list_food_get_idx_head(ListFood l){
 }
 
 /* IDX_UNDEF jika kosong, neff-1 jika idak kosong*/
-int list_food_get_idx_tail(ListFood l){
-    if (list_food_is_empty(l)){
+int list_food_get_idx_tail(ListFood l) {
+    if (list_food_is_empty(l)) {
         return IDX_UNDEF;
     } else {
-        return (NEFF(l)-1);
+        return (NEFF(l) - 1);
     }
+}
+
+/*
+Proses : Menambahkan capacity l sebanyak num
+I.S. List sudah terdefinisi
+F.S. Ukuran list bertambah sebanyak num */
+void expand_list_food(ListFood *l, int num) {
+    ListFood l2;
+    create_list_food(&l2, CAPACITY(*l) + num);
+    NEFF(l2) = NEFF(*l);
+    for (int i = 0; i < list_food_length(*l); i++) {
+        ELMT(l2, i) = ELMT(*l, i);
+    }
+    free(BUFFER(*l));
+    *l = l2;
+}
+
+/* Proses : Mengurangi capacity sebanyak num
+I.S. List sudah terdefinisi, ukuran capacity > num, dan nEff < capacity - num.
+F.S. Ukuran list berkurang sebanyak num. */
+void shrink_list_food(ListFood *l, int num) {
+    ListFood l2;
+    create_list_food(&l2, CAPACITY(*l) - num);
+    NEFF(l2) = NEFF(*l);
+    if (NEFF(l2) > CAPACITY(l2)) {
+        NEFF(l2) = CAPACITY(l2);
+    }
+    for (int i = 0; i < list_food_length(*l); i++) {
+        ELMT(l2, i) = ELMT(*l, i);
+    }
+    *l = l2;
+}
+
+/* Proses : Mengubah capacity sehingga nEff = capacity
+I.S. List tidak kosong
+F.S. Ukuran nEff = capacity */
+void compress_list_food(ListFood *l) {
+    shrink_list_food(l, CAPACITY(*l) - NEFF(*l));
 }
 
 /*
@@ -136,20 +177,23 @@ lain bila diperlukan
 Periksa is_full!
 jika penuh, panggil fungsi expand list
 */
-void enqueue_food(ListFood *l, food_t val){
-    if (list_food_is_full(*l)){
-        expand_list_food(l,CAPACITY(*l));
+void enqueue_food(ListFood *l, food_t val) {
+    if (list_food_is_full(*l)) {
+        expand_list_food(l, CAPACITY(*l));
     }
     int i = list_food_length(*l);
-    while (i>0){
-        if (TGT(FOOD_EXPIRE_TIME(ELMT(*l,i-1)),FOOD_EXPIRE_TIME(val))){
-            ELMT(*l,i) = ELMT(*l,i-1);
-        } else{
-            ELMT(*l,i) = val;
+    while (i > 0) {
+        if (TGT(FOOD_EXPIRE_TIME(ELMT(*l, i - 1)), FOOD_EXPIRE_TIME(val))) {
+            ELMT(*l, i) = ELMT(*l, i - 1);
+            i--;
+        } else {
+            ELMT(*l, i) = val;
+            NEFF(*l)++;
             return;
         }
     }
-    ELMT(*l,0) = val;
+    ELMT(*l, 0) = val;
+    NEFF(*l)++;
 }
 
 /*
@@ -161,13 +205,14 @@ Pastikan elemen tetap terurut sebelum dan sesudah proses dequeue
 return value 0 jika proses berjalan dengan lancar
 return value -1 jika tidak ditemukan makanan dengan id food_id atau jika list kosong
 */
-int dequeue_food(ListFood *l, int food_id, food_t *val){
+int dequeue_food(ListFood *l, int food_id, food_t *val) {
     int i = 0;
-    while(i<list_food_length(*l)){
-        if (ID(ELMT(*l,i))== food_id){
-            list_food_delete(l,i);
+    while (i < list_food_length(*l)) {
+        if (FOOD_ID(ELMT(*l, i)) == food_id) {
+            *val = ELMT(*l, i);
+            list_food_delete(l, i);
             return 0;
-        } else{ i++;}
+        } else { i++; }
     }
     return -1;
 }
@@ -176,60 +221,23 @@ int dequeue_food(ListFood *l, int food_id, food_t *val){
 Menghitung banyaknya makanan dengan id food_id pada list_food
 return nol jika tidak ada, return banyaknya jika ada
 */
-int food_count(ListFood l, int food_id){
-    int count =0;
-    for (int i =0;i<list_food_length(l);i++){
-        if (ID(ELMT(l,i))==food_id){
+int food_count(ListFood l, int food_id) {
+    int count = 0;
+    for (int i = 0; i < list_food_length(l); i++) {
+        if (FOOD_ID(ELMT(l, i)) == food_id) {
             count++;
         }
     }
     return count;
 }
 
-/*
-Proses : Menambahkan capacity l sebanyak num
-I.S. List sudah terdefinisi
-F.S. Ukuran list bertambah sebanyak num */
-void expand_list_food(ListFood *l, int num){
-    ListFood l2;
-    create_list_food(&l2, CAPACITY(*l) + num);
-    NEFF(l2) = NEFF(*l);
-    for(int i = 0; i < listLength(*l); i++){
-        ELMT(l2, i) = ELMT(*l, i);
-    }
-    free(BUFFER(*l));
-    *l = l2;
-}
-
-/* Proses : Mengurangi capacity sebanyak num
-I.S. List sudah terdefinisi, ukuran capacity > num, dan nEff < capacity - num.
-F.S. Ukuran list berkurang sebanyak num. */
-void shrink_list_food(ListFood *l, int num){
-    ListFood l2; 
-    create_list_food(&l2, CAPACITY(*l) - num);
-    NEFF(l2) = NEFF(*l);
-    if(NEFF(l2) > CAPACITY(l2)){
-        NEFF(l2) = CAPACITY(l2);
-    }
-    for(int i = 0; i < list_food_length(*l); i++){
-        ELMT(l2, i) = ELMT(*l, i);
-    }
-    *l = l2;
-}
-
-/* Proses : Mengubah capacity sehingga nEff = capacity
-I.S. List tidak kosong
-F.S. Ukuran nEff = capacity */
-void compress_list_food(ListFood *l){
-    shrink_list_food(l,CAPACITY(*l)-NEFF(*l));
-}
-
 /* Proses : Menampilkan isi list ke layar */
-void display_list_delivery(ListFood l){
+void display_list_food(ListFood l) {
     printf("--------------------------------------\n");
-    for (int i =0;i<list_delivery_length(l);i++){
-        display_food(ELMT(l,i));
+    for (int i = 0; i < list_food_length(l); i++) {
+        display_food(ELMT(l, i));
         printf("--------------------------------------\n");
     }
 }
+
 #endif
