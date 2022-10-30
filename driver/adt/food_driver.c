ecf5c6be6ab5a10df18f9545ed6142263ddd3739
egijago <egiyangbaik@gmail.com>
2022-10-30 11:45:43 +0700

    update: list_delivery_driver, list_food_driver,

--- driver/adt/food_driver.c
+++ driver/adt/food_driver.c
@@ -4,11 +4,11 @@
 
 int main(){
     food_t food;
-    char food_name [] = {'R','o','t','i'};
+    char food_name [] = "Roti";
     day_time_t del_time;
     create_time(&del_time,4,2,0);
     day_time_t exp_time;
-    create_time(&del_time,4,2,0);
+    create_time(&exp_time,4,2,0);
 
     create_food(&food,1,char_to_string(food_name),exp_time,del_time,Chop);
     display_food(food);
