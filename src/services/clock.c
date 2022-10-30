24573d9479c58d49c1dd78257734d6551e689fa7
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2022-10-30 11:23:24 +0700

    clock done, perlu cek

--- src/services/clock.c
+++ src/services/clock.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include "clock.h"
+#include "simulator.h"
+#include "delivery.h"
+#include "../data/simulator.h"
+#include "../adt/list_delivery.h"
+#include "../adt/time.h"
+
+extern simulator_t simulator;
 
 /*
 Advance waktu sebanyak satu menit
@@ -8,4 +15,9 @@ todo:
 kurangi waktu kadaluarsa inventory player (simulator next tick)
 kurangi waktu delivery (delivery next tick)
 */
-void next_tick();
+void next_tick() {
+    pnext_n_minute(&simulator.time, 1); // waktu nambah 1 menit
+
+    simulator_next_tick();
+    delivery_next_tick();
+}
