232578433712b013fe6e2102d4aba61cda6cf892
Eugene316 <92687071+Eugene316@users.noreply.github.com>
2022-11-01 17:17:09 +0700

    refactor: remove redundant services and commands

--- src/commands/boil.h
+++
@@ -1,8 +0,0 @@
-#include "food.h"
-#include "recipe.h"
-#include "../services/history.h"
-#include "../data/simulator.h"
-#include "../data/configdata.h"
-
-/* Melakukan command boil pada makanan */
-void do_boil();
