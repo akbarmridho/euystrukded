cc770f8ff9117170c8ead581a93ac0f00c3cc015
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-11-02 11:41:09 +0700

    update catalog and cookbook display

--- src/commands/catalog.c
+++ src/commands/catalog.c
@@ -4,32 +4,25 @@
 void display_catalog() {
     printf("List Makanan\n");
     printf("(nama - durasi kadaluarsa - aksi yang diperlukan - delivery time)\n");
+
+    int counter = 1;
     for (int i = 0; i < neff(food_recipe); i++) {
-        printf("%d. ", i + 1);
-        print_string(food_recipe.contents[i].food.name);
-        printf(" - ");
-        if (food_recipe.contents[i].food.expire_time.dd == 0 && food_recipe.contents[i].food.expire_time.hh == 0 &&
-            food_recipe.contents[i].food.expire_time.mm == 0) {
-            printf("0");
-        } else {
-            write_fulltime(food_recipe.contents[i].food.expire_time);
-            printf(" - ");
-        }
         if (food_recipe.contents[i].food.source == Buy) {
+            printf("%d. ", counter++);
+            print_string(food_recipe.contents[i].food.name);
+            printf(" - ");
+
+            if (food_recipe.contents[i].food.expire_time.dd == 0 && food_recipe.contents[i].food.expire_time.hh == 0 &&
+                food_recipe.contents[i].food.expire_time.mm == 0) {
+                printf("0");
+            } else {
+                write_fulltime(food_recipe.contents[i].food.expire_time);
+                printf(" - ");
+            }
             printf("BUY - ");
             write_fulltime(food_recipe.contents[i].food.delivery_time);
+            printf("\n");
         }
-        if (food_recipe.contents[i].food.source == Mix) {
-            printf("MIX");
-        }
-        if (food_recipe.contents[i].food.source == Chop) {
-            printf("CHOP");
-        }
-        if (food_recipe.contents[i].food.source == Fry) {
-            printf("FRY");
-        }
-
-        printf("\n");
     }
 
     printf("\nEnter command: ");
--- src/commands/cookbook.c
+++ src/commands/cookbook.c
@@ -2,6 +2,46 @@
 
 
 void cookbook() {
-    display_lfr(food_recipe);
+    printf("List Resep\n");
+    printf("(aksi yang diperlukan - daftar bahan)\n");
+
+    int counter = 1;
+
+    for (int i = 0; i < list_tree_recipe.length; i++) {
+        Tree t = list_tree_recipe.content[i];
+
+        if (t->food.source != Buy) {
+            printf("   %d. ", counter++);
+            print_string(FOOD_NAME(t->food));
+            putchar('\n');
+            switch (FOOD_SOURCE(t->food)) {
+                case (Buy):
+                    break;
+                case (Chop):
+                    printf("      Chop - ");
+                    break;
+                case (Mix):
+                    printf("      Mix - ");
+                    break;
+                case (Fry):
+                    printf("      Fry - ");
+                    break;
+                case (Boil):
+                    printf("      Boil - ");
+                    break;
+            }
+
+            for (int j = 0; j < t->children_count; j++) {
+
+                print_string(FOOD_NAME(t->children[j]->food));
+                if (j != (t->children_count - 1)) {
+                    printf(" - ");
+                } else {
+                    putchar('\n');
+                };
+            }
+        }
+    }
+    
     printf("\nEnter command: ");
 }
